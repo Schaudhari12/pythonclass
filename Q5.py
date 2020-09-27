96720731a5eaff8b308389d4623d8c512cd5ff21
Schaudhari12 <67353118+Schaudhari12@users.noreply.github.com>
2020-09-27 14:04:53 -0700

    Add files via upload

---
+++ Q5.py
@@ -0,0 +1,9 @@
+table = input("Please enter your table number")
+print(f'Your table looks like {table}, cool!') 
+table = int(7)
+for i in range(1,11,1):
+    result = table * i
+    print(f'{table} * {i} = {result}')
+
+
+
